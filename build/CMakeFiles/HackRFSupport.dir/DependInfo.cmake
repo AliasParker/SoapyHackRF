959a3e200c81fe5f9a9c1b472819b926d2d5a1cd
AliasParker <120339173+AliasParker@users.noreply.github.com>
2024-07-30 15:50:33 -0600

    v8

---
+++ build/CMakeFiles/HackRFSupport.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mcfly/Documents/GitHub/srf/SoapyHackRF/HackRF_Registration.cpp" "CMakeFiles/HackRFSupport.dir/HackRF_Registration.cpp.o" "gcc" "CMakeFiles/HackRFSupport.dir/HackRF_Registration.cpp.o.d"
+  "/Users/mcfly/Documents/GitHub/srf/SoapyHackRF/HackRF_Session.cpp" "CMakeFiles/HackRFSupport.dir/HackRF_Session.cpp.o" "gcc" "CMakeFiles/HackRFSupport.dir/HackRF_Session.cpp.o.d"
+  "/Users/mcfly/Documents/GitHub/srf/SoapyHackRF/HackRF_Settings.cpp" "CMakeFiles/HackRFSupport.dir/HackRF_Settings.cpp.o" "gcc" "CMakeFiles/HackRFSupport.dir/HackRF_Settings.cpp.o.d"
+  "/Users/mcfly/Documents/GitHub/srf/SoapyHackRF/HackRF_Streaming.cpp" "CMakeFiles/HackRFSupport.dir/HackRF_Streaming.cpp.o" "gcc" "CMakeFiles/HackRFSupport.dir/HackRF_Streaming.cpp.o.d"
+  "/Users/mcfly/Documents/GitHub/srf/SoapyHackRF/build/Version.cpp" "CMakeFiles/HackRFSupport.dir/Version.cpp.o" "gcc" "CMakeFiles/HackRFSupport.dir/Version.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
